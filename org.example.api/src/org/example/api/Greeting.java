0d92ba1a1c13a933e11a521d7162de860ff871c0
A835421 <A835421@A835421-LTW7.cis.neustar.com>
2017-08-19 16:31:38 +0530

    nj

--- org.example.api/src/org/example/api/Greeting.java
+++ org.example.api/src/org/example/api/Greeting.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package org.example.api;
 
 public interface Greeting {
@@ -6,3 +7,13 @@ public interface Greeting {
 	String sayHello(String name);
 
 }
+=======
+package org.example.api;
+
+public interface Greeting {
+	
+	
+	String sayHello(String name);
+
+}
+>>>>>>> branch 'master' of git@github.com:ferrari9999/self.git
